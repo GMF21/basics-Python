eb474489520bca3522c471617ffa4792cb2f597a
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-12 23:52:16 +0000

    Update First lines of code.py

--- First lines of code.py
+++ First lines of code.py
@@ -2,7 +2,7 @@ print("hello world")  #print somethin on the screen
 
 print("*" * 10)  #print times 10
 
-fruits = ["banana", "morango", "manga", "maçã"] #array
+fruits = ["banana", "morango", "manga", "maçã"] #array ( list of something in this case is a list os fruits)
 
 for y in fruits:  #loop for , for the array
     print(y)
