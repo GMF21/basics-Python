cf097a058f7daad2c6b503ed2f5a4de866afe933
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-16 11:26:54 +0000

    Update introPython.py

--- introPython.py
+++ introPython.py
@@ -8,3 +8,27 @@ elif idade == 18:
     print("acabaste e fazer 18")
 else:
     print("maior de idade")
+
+#operadores aritemétricos
+altura = 170
+
+print(idade + 5)
+print(altura * 2)
+print(idade / 2)
+print(altura - 2)
+
+
+#operadores de comparação 
+
+nome = "joao"
+estudante = True
+
+print(idade > 18)
+print(idade == 18)
+print(idade != 18)
+print(idade <= 18)
+print(idade >= 18)
+print(idade < 18)
+print(idade >= 18 and nome)
+print(idade >= 18 or estudante)
+print(not estudante)
