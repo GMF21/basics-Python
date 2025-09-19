0b3f64a2e38c3bb22948ebaa1e4c90a9afa2fa0b
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-19 11:12:24 +0000

    Update doit.py

--- doit.py
+++ doit.py
@@ -1,15 +1,17 @@
 entrence = ["Mateus", "Gonçalo", "João", "Joana"]
 
-name = input(("Qual é o teu nome? "))
+name = input("Qual é o teu nome? ")
 
-idade = input(("Qual é a tua idade? "))
+idade = int(input("Qual é a tua idade? "))  
 
-for y in entrence:
-    if idade < 18:
-        print("és menor de idade, não tens acesso.")
-        break
-    elif name == entrence[0:3]:
+if idade < 18:
+    print("És menor de idade, não tens acesso.")
+else:
+    if name in entrence:  
         print("Acesso liberado")
+    else:
+        print("Nome não reconhecido, acesso negado.")
+
      
 
 
