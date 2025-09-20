cdd83218f862bdeb141877aefd5590fd37bf7ea3
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-20 17:19:15 +0000

    Update list_compras.py

--- list_compras.py
+++ list_compras.py
@@ -1,10 +1,13 @@
 lista = ["forno", "fogão", "água"]
 
-try:
+add = 0
 
-    add = int(input("queres adicionar um novo produto a tua lista? (1-sim/2-não)"))
-except ValueError:
-    print("Digita 1 para adicionar e 2 para não")
+while add not in (1,2):
+    try:
+
+        add = int(input("queres adicionar um novo produto a tua lista? (1-sim/2-não)"))
+    except ValueError:
+        print("Digita 1 para adicionar e 2 para não")
 
 if add == 1:
     produto = input(str("Nome do produto: "))
@@ -16,5 +19,7 @@ elif add == 2:
 quantidade = len(lista)
 x = ", ".join(lista)
 
-print(f"Tens {quantidade} na tua lista: {x}")
+print(f"Tens {quantidade} produtos na tua lista: {x}")
+
+
 
