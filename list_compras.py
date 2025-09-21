ccd412ceade9e301bc81ab0bb76f64a63d54e8ee
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-21 12:59:41 +0000

    Update list_compras.py

--- list_compras.py
+++ list_compras.py
@@ -1,25 +1,39 @@
-lista = ["forno", "fogão", "água"]
+lista = []
 
-add = 0
+def mostrar_quantidade():
+    return len(lista)
 
-while add not in (1,2):
-    try:
+def mostrar_produtos():
+    if len(lista) > 0:
+        return ", ".join(lista)
+    else:
+        return "nenhum produto"
 
-        add = int(input("queres adicionar um novo produto a tua lista? (1-sim/2-não)"))
-    except ValueError:
-        print("Digita 1 para adicionar e 2 para não")
+while True:
+    quantidade = mostrar_quantidade()
+    produtos = mostrar_produtos()
 
-if add == 1:
-    produto = input(str("Nome do produto: "))
-    lista.append(produto)
-elif add == 2:
-    print("não adicionaste nenhum produto a mais")
-    
+    add = int(input(f"Queres adicionar um produto à tua lista? Atualmente tens {quantidade} na tua lista (1-sim/2-não/3-remover produto): "))
 
-quantidade = len(lista)
-x = ", ".join(lista)
+    if quantidade == 0:
+        print("Não tens nenhum produto na tua lista")
 
-print(f"Tens {quantidade} produtos na tua lista: {x}")
+    if add == 1:
+        novo_produto = input("Nome produto: ")
+        lista.append(novo_produto)
+    elif add == 2:
+        print("Não adicionaste nenhum produto. A sair...")
+        break
+    elif add == 3:
+        if mostrar_quantidade() == 0:
+            print("Não há nada para remover.")
+        else:
+            print(f"Produtos disponíveis a remover: {mostrar_produtos()}")
+            remover = input("Qual o produto que queres remover da lista: ")
+            
+            if remover not in lista:
+                print(f"Esse produto não está na lista, produtos disponíveis: {mostrar_produtos()}")
+            else:
+                lista.remove(remover)
 
-
-
+print(f"Sua lista tem {mostrar_quantidade()} produtos: {mostrar_produtos()}")
