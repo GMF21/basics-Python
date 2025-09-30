186fb6942cc2bd8c19681ffff359adcca5d14825
GNC <145757613+GMF21@users.noreply.github.com>
2025-09-30 10:32:56 +0000

    Update pedraPapelTesoura.py

--- pedraPapelTesoura.py
+++ pedraPapelTesoura.py
@@ -2,22 +2,34 @@ import random
 
 lista = ["pedra", "papel", "tesoura"]
 
+contador = 0
+perdeste = 0
+empate = 0
+
 while True:
     pergunta = input("Pedra, Papel ou Tesoura (q - quit): ").lower()
 
     if pergunta == "q":
-        print("Jogo encerrado.")
+        print("A sair...")
         break
 
     sorteio = random.choice(lista)
 
+    
     if (sorteio == "pedra" and pergunta == "papel") or \
        (sorteio == "papel" and pergunta == "tesoura") or \
        (sorteio == "tesoura" and pergunta == "pedra"):
-        print("Ganhaste!")
+        print("win")
+        contador += 1
+        
+
     elif sorteio == pergunta:
-        print("Empate!")
+        print("Empate")
+        empate += 1
     else:
-        print("Perdeste!")
+        print("Perdeste")
+        perdeste += 1
 
     print(f"O computador escolheu: {sorteio}")
+
+    print(f"Ganhaste {contador} vezes, e perdeste {perdeste} vezes e empataste {empate} vezes")
